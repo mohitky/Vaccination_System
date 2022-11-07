a1cbc76da56ffae4f451a335b9b6d344e42b6920
Mohit Yadav <82125834+mohitky@users.noreply.github.com>
2022-11-07 14:35:22 +0530

    Update ConnectionProvider.java
    1

--- src/com/project/ConnectionProvider.java
+++ src/com/project/ConnectionProvider.java
@@ -14,7 +14,7 @@ public class ConnectionProvider {
 
                 // Change user_name and password ....with your respective database user name and password
                 String user_name = "root";
-                String password = "mohit7091";
+                String password = "root";
                 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/vaccineproject",user_name,password);
 
             }
